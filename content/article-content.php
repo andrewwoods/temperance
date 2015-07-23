e796666995983c5e10e3d4be247dfb15b7c4585b
Andrew Woods <atwoods1@gmail.com>
2015-07-22 18:46:05 -0700

    Moved article content to a reusable template part.

---
+++ content/article-content.php
@@ -0,0 +1,35 @@
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'h-entry' ); ?> role="article">
+
+	<header class="article-header">
+
+		<h1><a href="<?php the_permalink() ?>" class="p-name" rel="bookmark"><?php the_title(); ?></a></h1>
+		<p class="byline vcard">
+		<?php
+			$time_tag = '<time class="dt-published" datetime="%1$s" pubdate>%2$s</time>';
+
+			printf( __( "Posted $time_tag", 'temperancetheme' ),
+				get_the_time( 'c' ),
+				get_the_time( get_option( 'date_format' ) )
+			);
+			printf( __( '<span class="author p-author">%1$s</span>
+				<span class="amp">&</span> filed under %2$s.', 'temperancetheme' ),
+				temperance_get_the_author_posts_link(),
+				get_the_category_list(', ')
+			);
+		?>
+		</p>
+
+	</header>
+
+	<section class="entry-content e-content">
+		<?php the_content(); ?>
+	</section>
+
+	<footer class="article-footer">
+		<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'temperancetheme' ) . '</span> ', ', ', '' ); ?></p>
+
+	</footer>
+
+	<?php // comments_template(); // uncomment if you want to use them ?>
+</article>
--- index.php
+++ index.php
@@ -3,32 +3,9 @@
 <div id="content">
 	<div id="inner-content" class="wrap clearfix">
 		<div id="main" class="eightcol first clearfix" role="main">
-
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
-
-				<header class="article-header">
-
-					<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-						<p class="byline vcard"><?php
-							printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'temperancetheme' ), get_the_time( 'c' ), get_the_time( get_option( 'date_format' ) ), temperance_get_the_author_posts_link(), get_the_category_list(', '));
-					?></p>
-
-				</header>
-
-				<section class="entry-content clearfix">
-					<?php the_content(); ?>
-				</section>
-
-				<footer class="article-footer">
-					<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'temperancetheme' ) . '</span> ', ', ', '' ); ?></p>
-
-				</footer>
-
-				<?php // comments_template(); // uncomment if you want to use them ?>
-			</article>
-
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+			get_template_part( 'article', 'content' );
+		?>
 			<?php endwhile; ?>
 
 				<?php if ( function_exists( 'temperance_page_navi' ) ) { ?>
