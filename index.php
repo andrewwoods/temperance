f203a753532a4a7325d645f5b618084de2795e39
Andrew Woods <atwoods1@gmail.com>
2015-10-01 23:06:24 -0700

    Make minor markup tweaks
    Fixed path in get_template_part.
    Removed unnecessary markup from the 'not found' section.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<div id="inner-content" class="wrap clearfix">
 		<div id="main" class="eightcol first clearfix" role="main">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-			get_template_part( 'article', 'content' );
+			get_template_part( 'content/article', 'content' );
 		?>
 			<?php endwhile; ?>
 
@@ -22,17 +22,10 @@
 			<?php else : ?>
 
 				<article id="post-not-found" class="hentry clearfix">
-					<header class="article-header">
-						<h1><?php _e( 'Oops, Post Not Found!', 'temperancetheme' ); ?></h1>
-					</header>
-
 					<section class="entry-content">
 						<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'temperancetheme' ); ?></p>
 					</section>
 
-					<footer class="article-footer">
-						<p><?php _e( 'This is the error message in the index.php template.', 'temperancetheme' ); ?></p>
-					</footer>
 				</article>
 
 			<?php endif; ?>
