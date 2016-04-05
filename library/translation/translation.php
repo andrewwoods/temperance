a9e7e685e66e3fe2b7910b5e0ce4b6e4bd4235a8
Andrew Woods <atwoods1@gmail.com>
2016-04-04 17:20:46 -0700

    Code Cleanup

--- library/translation/translation.php
+++ library/translation/translation.php
@@ -1,12 +1,17 @@
 <?php
-/* Welcome to Temperance :)
-Thanks to the fantastic work by Temperance users, we've now
-the ability to translate Temperance into different languages.
+/*
+	Lanaguage files are used to translate the interface of your WordPress website
+	into multiple languages. To add support for a new language, put the translation files
+	into the 'library/translation' directory.
 
+	Work with native speakers to create translation files.
+
+	DO NOT USE AUTOMATED SERVICES like Google Translate.
 */
 
+add_action('after_setup_theme', 'temperanace_theme_setup');
 
-
-// Adding Translation Option
-load_theme_textdomain( 'temperancetheme', get_template_directory() .'/library/translation' );
+function my_theme_setup(){
+	load_theme_textdomain( 'temperancetheme', get_template_directory() .'/library/translation' );
+}
 
