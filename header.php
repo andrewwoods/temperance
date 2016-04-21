721b2fe915991e016503b1430d87a786fa2023e4
Andrew Woods <atwoods1@gmail.com>
2016-04-21 11:57:36 -0700

    Clean up head of the document
    * Modernized the page header since Microsoft's Edge browser replaces IE.
    * We don't need the function to add wp_title() since theme support for
        the title-tag is enabled

--- header.php
+++ header.php
@@ -1,42 +1,15 @@
-<!doctype html>
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-
-		<?php // Google Chrome Frame for IE ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<?php
-		if ( ! function_exists( '_wp_render_title_tag' ) ) {
-			function theme_slug_render_title() {
-		?>
-				<title><?php wp_title( ' | ', true, 'right' ); ?></title>
-			<?php
-			}
-			add_action( 'wp_head', 'theme_slug_render_title' );
-		}
-		?>
-
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<![endif]-->
-		<?php // or, set /favicon.ico for IE10 win ?>
-		<meta name="msapplication-TileColor" content="#f16c00">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
 		<?php wp_head(); ?>
-
 	</head>
 
 	<body <?php body_class(); ?>>
