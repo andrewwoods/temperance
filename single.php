0eb68f8228b4cde84a3adac54ba3983394145cfa
Andrew Woods <atwoods1@gmail.com>
2019-10-15 17:23:00 -0400

    Update to use template parts in 'content/'

--- single.php
+++ single.php
@@ -11,30 +11,14 @@
 <main id="main" class="main" role="main">
 	<div id="main-content" class="main-content">
 
-	<?php
+        <?php
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-			get_template_part( 'post', 'content' );
+			get_template_part( 'content/content' );
 		endwhile;
 	else :
-		$not_found = __( 'Oops, Post Not Found!', 'temperancetheme' );
-		$error     = __( 'This is the error message in single.php', 'temperancetheme' );
-		$missing   = __( 'Uh Oh. Something is missing. ', 'temperancetheme' );
-		?>
-		<article id="post-not-found" class="hentry clearfix">
-			<header class="article-header">
-				<h1><?php echo $not_found; ?></h1>
-			</header>
-			<section class="entry-content">
-				<p><?php echo $missing; ?></p>
-			</section>
-			<footer class="article-footer">
-				<p><?php echo $error; ?></p>
-			</footer>
-		</article>
-
-	<?php
+		get_template_part( 'content/empty' );
 	endif;
 	?>
 
